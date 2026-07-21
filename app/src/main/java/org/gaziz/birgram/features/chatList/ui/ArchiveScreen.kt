8ab5fcf991a4f240fc4b92e938a2e94ecbe077e1
GAZIZlikesLollipop <gazizvalitov230378@gmail.com>
2026-07-21 17:55:53 +0500

    feat(chatList): top bar для ArchiveScreen

--- app/src/main/java/org/gaziz/birgram/features/chatList/ui/ArchiveScreen.kt
+++ app/src/main/java/org/gaziz/birgram/features/chatList/ui/ArchiveScreen.kt
@@ -1,21 +1,65 @@
 package org.gaziz.birgram.features.chatList.ui
 
-import androidx.activity.compose.BackHandler
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import org.gaziz.birgram.R
+import org.gaziz.birgram.core.ui.icons.arrowBack
+import org.gaziz.birgram.core.ui.icons.moreVert
 
 @Composable
 fun ArchiveScreen(
     onBack: () -> Unit
 ) {
-    BackHandler { onBack() }
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colorScheme.background)
-    )
+    val archivedChats = stringResource(R.string.archived_chats)
+    Scaffold(
+        topBar = {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(WindowInsets.statusBars.asPaddingValues()),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                IconButton(
+                    onClick = onBack
+                ) {
+                    Icon(
+                        imageVector = arrowBack,
+                        contentDescription = null
+                    )
+                }
+                Text(
+                    text = archivedChats,
+                    style = MaterialTheme.typography.labelSmall
+                )
+                IconButton(
+                    onClick = {}
+                ) {
+                    Icon(
+                        imageVector = moreVert,
+                        contentDescription = null
+                    )
+                }
+            }
+        }
+    ) { paddingValues ->
+        LazyColumn(
+            modifier = Modifier.padding(paddingValues)
+        ) {}
+    }
 }
--- app/src/main/java/org/gaziz/birgram/features/chatList/ui/ChatListViewModel.kt
+++ app/src/main/java/org/gaziz/birgram/features/chatList/ui/ChatListViewModel.kt
@@ -10,7 +10,6 @@ import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import org.gaziz.birgram.core.datastore.UserPreferencesRepository
 import org.gaziz.birgram.core.telegram.api.AuthService
-import org.gaziz.birgram.core.telegram.api.ChatService
 import org.gaziz.birgram.core.telegram.api.UserService
 import org.gaziz.birgram.core.telegram.api.model.chat.ChatListType
 import org.gaziz.birgram.core.telegram.api.usecase.DownloadChatPhotoSmall
@@ -24,7 +23,6 @@ class ChatListViewModel @Inject constructor(
     loadChatList: LoadChatList,
     getChatList: GetChatList,
     userService: UserService,
-    chatService: ChatService,
     private val getAccentColorById: GetAccentColorById,
     private val downloadChatPhotoSmall: DownloadChatPhotoSmall,
     private val authService: AuthService,
