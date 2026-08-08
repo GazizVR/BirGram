9a416680066093b5e7fb950a1f28a5132a4303b0
GAZIZlikesLollipop <gazizvalitov230378@gmail.com>
2026-08-08 21:47:07 +0500

    feat(chat): пузрь для DoucmentPreview in ui

--- app/src/main/java/org/gaziz/birgram/features/chat/ui/component/messageContent/DocumentPreview.kt
+++ app/src/main/java/org/gaziz/birgram/features/chat/ui/component/messageContent/DocumentPreview.kt
@@ -1,6 +1,8 @@
 package org.gaziz.birgram.features.chat.ui.component.messageContent
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -8,10 +10,17 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -30,14 +39,37 @@ fun DocumentPreview(
 ) {
     val fontSize = 6.sp
     val shape = RoundedCornerShape(16.dp)
+    val isDownloading by rememberSaveable { mutableStateOf(false) }
     Box(
         modifier = Modifier
             .clip(shape)
             .background(containerColor,shape)
     ) {
         Row (
-            modifier = Modifier.padding(8.dp)
-        ){
+            modifier = Modifier.padding(12.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            val interactionSource = remember { MutableInteractionSource() }
+            Box(
+                modifier = Modifier
+                    .size(40.dp)
+                    .clip(CircleShape)
+                    .background(MaterialTheme.colorScheme.onBackground)
+                    .clickable(
+                        indication = null,
+                        interactionSource = interactionSource
+                    ) {
+
+                    },
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator(
+                    color = containerColor,
+                    modifier = Modifier.size(20.dp),
+                    strokeWidth = 2.dp
+                )
+            }
             Column(
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.Start
@@ -56,37 +88,41 @@ fun DocumentPreview(
                     document.size != null ||
                     document.type != null
                 ) {
-                    Spacer(Modifier.height(3.dp))
+                    Spacer(Modifier.height(4.dp))
                     var text = ""
                     if(document.size != null) text += document.size
                     if(document.type != null) text += " ${document.type}"
-                    Text(
-                        text = text,
-                        overflow = TextOverflow.Ellipsis,
-                        fontSize = fontSize,
-                        lineHeight = fontSize,
-                        fontWeight = FontWeight.Thin,
-                        maxLines = 1
-                    )
-                }
-                Row {
-                    if(
-                        document.size != null ||
-                        document.type != null ||
-                        document.fileName != null
-                    ) {
-                        Spacer(Modifier.weight(1f))
+                    Box {
+                        Row {
+                            Text(
+                                text = text,
+                                overflow = TextOverflow.Ellipsis,
+                                fontSize = fontSize,
+                                lineHeight = fontSize,
+                                fontWeight = FontWeight.Thin,
+                                maxLines = 1,
+                                modifier = Modifier.weight(1f)
+                            )
+                            Text(
+                                text = "24:35",
+                                color = Color.Transparent,
+                                fontSize = fontSize,
+                                lineHeight = fontSize,
+                                fontWeight = FontWeight.Thin,
+                                maxLines = 1
+                            )
+                        }
                     }
-                    Text(
-                        text = date,
-                        color = MaterialTheme.colorScheme.onBackground.copy(0.5f),
-                        fontSize = fontSize,
-                        lineHeight = fontSize,
-                        fontWeight = FontWeight.Thin,
-                        maxLines = 1
-                    )
                 }
             }
         }
+        Text(
+            text = date,
+            color = MaterialTheme.colorScheme.onBackground.copy(0.5f),
+            fontSize = 5.sp,
+            lineHeight = 5.sp,
+            modifier = Modifier.align(Alignment.BottomEnd).padding(8.dp),
+            maxLines = 1
+        )
     }
 }
--- app/src/main/java/org/gaziz/birgram/features/chat/ui/component/messageContent/TextPreview.kt
+++ app/src/main/java/org/gaziz/birgram/features/chat/ui/component/messageContent/TextPreview.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.selection.DisableSelection
@@ -13,10 +12,6 @@ import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -58,15 +53,13 @@ fun TextPreview(
             Box {
                 SelectionContainer {
                     Row {
-                        var isSingleLine by rememberSaveable { mutableStateOf(false) }
                         Text(
                             text = text,
                             color = MaterialTheme.colorScheme.onBackground,
                             fontSize = fontSize,
-                            onTextLayout = { isSingleLine = it.lineCount < 2 },
-                            lineHeight = fontSize
+                            lineHeight = fontSize,
+                            modifier = if(isSpacer) Modifier.weight(1f) else Modifier
                         )
-                        if(isSpacer && isSingleLine) Spacer(Modifier.weight(1f))
                         DisableSelection {
                             Text(
                                 text = " 24:32",
