496c585cb7a36e6f82e30b2dd712cb0b75a20680
GAZIZlikesLollipop <gazizvalitov230378@gmail.com>
2026-09-11 19:25:20 +0500

    fix(chatlist): add serialization plugin in build.gradle.kts file

--- feature/chat-list/build.gradle.kts
+++ feature/chat-list/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.compose)
     id("com.google.devtools.ksp") version "2.3.7"
     id("com.google.dagger.hilt.android")
+    kotlin("plugin.serialization") version "2.0.21"
 }
 
 android {
