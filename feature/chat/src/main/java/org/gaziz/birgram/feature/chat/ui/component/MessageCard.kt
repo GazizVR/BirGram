143daefd612d0b0ccb826cf68aa9b835366d0f53
GAZIZlikesLollipop <gazizvalitov230378@gmail.com>
2026-09-25 19:00:56 +0500

    feat(chat): add DropDownMenu with CopyButton

--- feature/chat/src/main/java/org/gaziz/birgram/feature/chat/ui/component/MessageCard.kt
+++ feature/chat/src/main/java/org/gaziz/birgram/feature/chat/ui/component/MessageCard.kt
@@ -1,5 +1,7 @@
 package org.gaziz.birgram.feature.chat.ui.component
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
@@ -8,15 +10,23 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import org.gaziz.birgram.core.ui.component.ChatAvatar
+import org.gaziz.birgram.feature.chat.ui.component.dropDownContent.CopyButton
 import org.gaziz.birgram.feature.chat.ui.component.messageContent.ContentPreview
+import org.gaziz.birgram.feature.chat.ui.model.MessageContentInfo
 import org.gaziz.birgram.feature.chat.ui.model.MessageUiState
 
 @Composable
@@ -30,15 +40,21 @@ fun MessageCard(
         MaterialTheme.colorScheme.surfaceContainerLow
     }
     val spacerSize = 40.dp
-    Row(
+    var expanded by remember { mutableStateOf(false) }
+    Box(
         modifier = Modifier
             .fillMaxWidth()
             .padding(
                 horizontal = 8.dp,
                 vertical = 4.dp
-            ),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = if(message.isOutgoing) Arrangement.End else Arrangement.Start
+            )
+            .clickable(
+                indication = null,
+                interactionSource = remember { MutableInteractionSource() }
+            ) {
+                expanded = true
+            },
+        contentAlignment = if(message.isOutgoing) Alignment.CenterEnd else Alignment.CenterStart,
     ) {
         Row(
             verticalAlignment = Alignment.Bottom,
@@ -80,6 +96,26 @@ fun MessageCard(
             }
             if(!message.isOutgoing) {
                 Spacer(Modifier.width(spacerSize))
+            }
+        }
+        Box(
+            contentAlignment = Alignment.Center
+        ) {
+            DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false },
+                containerColor = MaterialTheme.colorScheme.secondaryContainer,
+                shape = RoundedCornerShape(8.dp),
+            ) {
+                if(
+                    message.content is MessageContentInfo.Text
+                ) {
+                    CopyButton(
+                        cnt = message.content,
+                        fontSize = fontSize,
+                        onClick = { expanded = false }
+                    )
+                }
             }
         }
     }
---
+++ feature/chat/src/main/java/org/gaziz/birgram/feature/chat/ui/component/dropDownContent/CopyButton.kt
@@ -0,0 +1,56 @@
+package org.gaziz.birgram.feature.chat.ui.component.dropDownContent
+
+import android.content.ClipData
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.platform.ClipEntry
+import androidx.compose.ui.platform.LocalClipboard
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.TextUnit
+import kotlinx.coroutines.launch
+import org.gaziz.birgram.core.ui.icon.contentCopy
+import org.gaziz.birgram.feature.chat.R
+import org.gaziz.birgram.feature.chat.ui.model.MessageContentInfo
+
+@Composable
+fun CopyButton(
+    cnt: MessageContentInfo,
+    fontSize: TextUnit,
+    onClick: () -> Unit,
+) {
+    val clipboardManager = LocalClipboard.current
+    val scope = rememberCoroutineScope()
+    DropdownMenuItem(
+        text = {
+            Text(
+                text = stringResource(R.string.copy),
+                fontSize = fontSize,
+                lineHeight = fontSize,
+                color = MaterialTheme.colorScheme.onBackground
+            )
+        },
+        leadingIcon = {
+            Icon(
+                imageVector = contentCopy,
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.onBackground,
+            )
+        },
+        onClick = {
+            scope.launch {
+                val text = when(cnt) {
+                    is MessageContentInfo.Text -> cnt.text
+                    else -> ""
+                }
+                val data = ClipData.newPlainText("", text)
+                val clip = ClipEntry(data)
+                clipboardManager.setClipEntry(clip)
+            }
+            onClick()
+        }
+    )
+}
